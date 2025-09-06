7350939bb42996754a333927dcb15dd8fc76f593
unknown <gavino_10@hotmail.es>
2025-09-06 18:22:19 -0500

    second-commit

--- src/main/java/com/springbootmvc/crudspringbootmvc/security/SpringSecurityConfig.java
+++ src/main/java/com/springbootmvc/crudspringbootmvc/security/SpringSecurityConfig.java
@@ -1,9 +1,15 @@
 package com.springbootmvc.crudspringbootmvc.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SpringSecurityConfig {
 
-
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/springbootmvc/crudspringbootmvc/services/UserServiceImpl.java
+++ src/main/java/com/springbootmvc/crudspringbootmvc/services/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.springbootmvc.crudspringbootmvc.services;
 
 import com.springbootmvc.crudspringbootmvc.entities.User;
+import com.springbootmvc.crudspringbootmvc.repositories.RoleRepository;
 import com.springbootmvc.crudspringbootmvc.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,21 +12,25 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository repository;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository repository) {
-        this.repository = repository;
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional(readOnly = true)
     @Override
     public List<User> findAll() {
-        return (List<User>)  this.repository.findAll();
+        return (List<User>)  this.userRepository.findAll();
     }
 
     @Transactional
     @Override
     public User save(User user) {
-        return this.repository.save(user);
+        return this.userRepository.save(user);
     }
 }
